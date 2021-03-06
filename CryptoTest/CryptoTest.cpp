fd48940bc9027ffd1e381ec743073857059b4bdd
yangjiang <yangjiang0217@126.com>
2021-03-06 14:05:12 +0800

    加密模块测试程序上传。

---
+++ CryptoTest/CryptoTest.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include "../Crypto/CryptoApi.h"
+#ifdef _WIN32
+#ifdef _DEBUG
+#pragma comment(lib,"../lib/Debug/Crypto.lib")
+#else
+#pragma comment(lib,"../lib/Release/Crypto.lib")
+#endif
+#endif
+
+int main()
+{
+    std::cout << "Please input text:" << std::endl;
+    std::string strText = "";
+    std::cin >> strText;
+
+    std::cout << "Please input key:" << std::endl;
+    std::string strKey = "";
+    std::cin >> strKey;
+
+    char szEncrypt[128] = { 0 };
+    char szDecrypt[128] = { 0 };
+    CRYPTO_EncryptString(strText.c_str(), strKey.c_str(), szEncrypt, sizeof(szEncrypt));
+    std::cout << szEncrypt << "\n";
+
+    CRYPTO_DecryptString(szEncrypt, strKey.c_str(), szDecrypt, sizeof(szDecrypt));
+    std::cout << szDecrypt << "\n";
+    system("pause");
+    return 0;
+}
